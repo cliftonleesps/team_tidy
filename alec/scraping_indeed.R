fe673096338119413631bbe70801f8b9c234a161
Alec McCabe <alec.mccabe93@gmail.com>
2021-10-15 19:14:52 -0400

    added skills extraction

--- alec/scraping_indeed.R
+++ alec/scraping_indeed.R
@@ -67,32 +67,61 @@ get_href_list <- function(jobs_list) {
 
 
 pull_page_results <- function(job_href_list) {
-  job_descriptions <- list()
+  job_title_list = list()
+  company_name_list = list()
+  state_list = list()
+  job_description_list = list()
   
   for (i in seq(job_href_list)){
     new_url <- str_c("https://indeed.com", job_href_list[i])
     #sel_driver$navigate(new_url)
     #new_page <- sel_driver$getPageSource()[[1]]
     new_page <- xml2::read_html(new_url)
-    viewJobSSRRoot <- rvest::html_element(new_page, 'div[id="viewJobSSRRoot"]')
-    full_text <- viewJobSSRRoot %>% html_text2()
-    job_descriptions <- append(job_descriptions, full_text)
+    
+    job_title <- html_element(new_page, 'h1[class="icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title"]') %>%
+      html_text2()
+    
+    company <- html_element(new_page, 'div[class="jobsearch-CompanyReview--heading"]') %>%
+      html_text2()
+    
+    job_description_text <- html_element(new_page, 'div[id="jobDescriptionText"]') %>%
+      html_text2()
+    
+    #viewJobSSRRoot <- rvest::html_element(new_page, 'div[id="viewJobSSRRoot"]')
+    #full_text <- viewJobSSRRoot %>% html_text2()
+    
+    job_title_list <- append(job_title_list, job_title)
+    company_name_list <- append(company_name_list, company)
+    state_list <- append(state_list, state_string)
+    job_description_list <- append(job_description_list, job_description_text)
   }
   
-  return(job_descriptions)
+  ret = list(job_title_list,company_name_list,state_list,job_description_list)
+  
+  return(ret)
 }
 
 
 
-traverse_job_search <- function(base_page, depth){
-  ret_list <- list()
+traverse_job_search <- function(base_page, state_string, depth){
+  job_title_list = list()
+  company_name_list = list()
+  state_list = list()
+  job_description_list = list()
+  
   
   for (i in seq(depth)){
     print(str_interp("page ${i}"))
     jobs <- get_jobs(base_page)
     page_url <- get_page_url(base_page)
     href_list <- get_href_list(jobs)
-    job_descriptions <- pull_page_results(href_list)
+    page_results <- pull_page_results(href_list)
+    
+    job_title_list <- append(job_title_list, page_results[1])
+    job_title_list <- append(job_title_list, page_results[1])
+    job_title_list <- append(job_title_list, page_results[1])
+    job_title_list <- append(job_title_list, page_results[1])
+    
     ret_list <- append(ret_list, job_descriptions)
     #sel_driver$navigate(page_url)
     #base_page <-sel_driver$getPageSource()[[1]]
@@ -121,14 +150,39 @@ get_multiple_state_jobs <- function(state_list, depth) {
   
   for (state in state_list) {
     print(state)
-    state_results <- get_state_jobs(state,depth=depth, sel_driver=sel_driver)
-    state_count <- re(state, 15)
+    state_results <- get_state_jobs(state,depth=depth)
     final_results <- append(final_results, state_results)
     final_states <- append(final_states, state_count)
   }
   
   return(c(final_states,final_results))
 }
+
+
+page <- get_base_page("New-York")
+
+jobs <- get_jobs(page)
+
+href_list <- get_href_list(jobs)
+
+sample_job_string <-str_c("https://indeed.com", href_list[1])
+
+sample_job <- xml2::read_html(sample_job_string)
+
+company <- html_element(sample_job, 'div[class="jobsearch-CompanyReview--heading"]') %>%
+            html_text2()
+
+job_title <- html_element(sample_job, 'h1[class="icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title"]') %>%
+  html_text2()
+
+job_description_text <- html_element(sample_job, 'div[id="jobDescriptionText"]') %>%
+  html_text2()
+
+data <- rvest::html_element(sample_job, 'div[id="viewJobSSRRoot"]')
+
+
+
+
 
 state_list <- c("New-York",
                 "Boston",
@@ -152,79 +206,5 @@ first_job <- jobs_df[1,]
 
 write.csv(jobs_df, file = "sample_new_york_jobs.csv",
           row.names = FALSE)
-
-
-# Extract the page_scroller object, we will need this to iterate 
-#through multiple jobcards
-
-
-
-# Begin to iterate through jobs, extracting relevant job_urls into a list
-
-
-
-
-
-
-# for each url in the href_list, crawl to the page and 
-# extract job text, store in a list
-
-
-
-
-
-
-
-
-
-
-# above apporach won't work because they use masking
-# instead we will try selenium
-
-
-
-
-temp_url <- str_c("https://indeed.com",href_list[1])
-
-remDr$navigate(temp_url)
-
-html <- remDr$getPageSource()[[1]]
-
-signals <- read_html(html)
-
-viewJobSSRRoot <- rvest::html_element(signals, 'div[id="viewJobSSRRoot"]')
-
-rvest::html_text2(viewJobSSRRoot)
-
-
-
-title <- viewJobSSRRoot %>% rvest::html_nodes(".jobsearch-JobInfoHeader-title-container ") %>%
-  rvest::html_text2()
-
-viewJobSSRRoot %>% 
-  rvest::html_node(".jobsearch-JobComponent-description icl-u-xs-mt--md")
-
-#rvest::html_node("#mosaic-aboveExtractedJobDescription")
-
-%>%
-  rvest::html_node(".mosaic js-match-insights-provider mosaic-provider-hydrated")
-
-viewJobSSRRoot %>%
-  rvest::html_element('div[class="icl-Container--fluid fs-unmask jobsearch-ViewJobLayout-fluidContainer is-US icl-u-xs-p--sm"]') %>%
-  rvest::html_element('div[class="icl-Grid jobsearch-ViewJobLayout-content jobsearch-ViewJobLayout-mainContent icl-u-lg-mt--md"]') %>%
-  rvest::html_element('div[class="jobsearch-ViewJobLayout-innerContent icl-Grid-col icl-u-xs-span12 icl-u-lg-offset2 icl-u-lg-span10"]') %>%
-  rvest::html_element('div[class="icl-Grid jobsearch-ViewJobLayout-innerContentGrid"]') %>%
-  rvest::html_element('div[class="jobsearch-ViewJobLayout-jobDisplay icl-Grid-col icl-u-xs-span12 icl-u-lg-span7"]')
-  
-  
-  rvest::html_element('div[class="jobsearch-DesktopStickyContainer is-sticky"]')
-
-
-## collect all <a> into list, these are the job bords
-## for each <a>, extract and crawl to the href url
-
-new_url
-
-
 
 
