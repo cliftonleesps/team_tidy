39f5367618a4f5f89a3d1c8865c086d8fb8e36c5
Alec McCabe <alec.mccabe93@gmail.com>
2021-10-16 11:37:53 -0400

    fixed emsi api

--- alec/emsi_skils_extraction.R
+++ alec/emsi_skils_extraction.R
@@ -3,6 +3,10 @@ library(jsonlite)
 library(tidyverse)
 library(stringr)
 
+client_id_2 <- "duwgu3vuhlbr3vj2"
+secret_2 <- "RWY09R8M"
+scope_2 <- "emsi_open"
+
 client_id <- '9doiv2cmqlaf9r0f'
 secret <- 'KTefV00m'
 scope <-'emsi_open'
@@ -35,7 +39,7 @@ get_token <- function(client_id, secret, scope){
   return(access_token)
 }
 
-access_token <- get_token(client_id,secret,scope)
+access_token <- get_token(client_id_2,secret_2,scope_2)
 
 # get indeed data
 
@@ -49,25 +53,26 @@ colnames(data) <- c("job_title","company_name","state","description")
 get_skills <- function(job_description, confidence_string, access_token){
   url <- "https://emsiservices.com/skills/versions/latest/extract"
   clean_description <- str_replace_all(job_description,"\n","")
-  payload <- str_interp("{ \"text\": \"${clean_description}\", \"confidenceThreshold\": ${confidence_string} }")
+  payload <- str_c("{ \"text\": \"... ", clean_description, " ...\", \"confidenceThreshold\": ", confidence_string, " }")
+  token_string <- str_interp('authorization: Bearer ${access_token}')
   encode <- "json"
-  token_string <- str_interp('Authorization: Bearer ${access_token}')
   
   response <- VERB("POST", 
                     url, 
-                    body = payload, 
+                    body = payload,
                     add_headers(
                             Authorization = token_string, 
                             Content_Type = 'application/json'), 
-                    content_type("application/json"), 
-                    encode = encode)
+                    content_type("application/json"),
+                   encode=encode
+                   )
   
   response_text <- content(response, "text")
   
   response_json <- fromJSON(response_text)
   
-  skill_type <- skills_json$data$skill$type$name
-  skill_name <- skills_json$data$skill$name
+  skill_type <- response_json$data$skill$type$name
+  skill_name <- response_json$data$skill$name
   
   skill_df <- as_tibble(skill_name)
   colnames(skill_df) <- c("skill")
@@ -81,12 +86,28 @@ get_skills <- function(job_description, confidence_string, access_token){
   return(skill_df)
 }
 
-test_job <- data$description[10]
+test_job <- data$description[16]
 
-skills_df <- get_skills(test_job, "0.9", access_token)
+test_job
+
+new_test<- get_skills(test_job, "0.1", access_token)
 
 create_skills_df <- function(job_title, company_name, state, description, confidence_threshold, access_token){
+  base_df <- tibble(
+    job_title = character(),
+    company_name = character(),
+    state = character(),
+    description = character(),
+    skill = character(),
+    type = character()
+  )
+  
   skills_df <- get_skills(description, confidence_threshold, access_token)
+  
+  if (length(skills_df)==0){
+    print("error with job")
+    return(base_df)
+  }
   
   skills_df <- skills_df %>%
                   mutate(
@@ -101,6 +122,8 @@ create_skills_df <- function(job_title, company_name, state, description, confid
   return(skills_df)
 }
 
+length(new_test)
+
 job_title = data[1,"job_title"][[1]]
 company_name = data[1,"company_name"][[1]]
 state = data[1,"state"][[1]]
@@ -112,6 +135,8 @@ test_df <- create_skills_df(job_title,
                             description, 
                             "0.6",
                             access_token)
+
+description
 
 
 
@@ -150,11 +175,13 @@ get_dataset_skills <- function(data, confidence_threshold, access_token){
 all_skills_df <- get_dataset_skills(data, "0.7",access_token)
 
 
-write.csv(all_skills_df, file = "indeed_skills_df.csv",
+write.csv(all_skills_df, file = "indeed_skills_df_fixed.csv",
           row.names = FALSE)
 
-test_read <- read_csv("indeed_skills_df.csv")
+test_read <- read_csv("indeed_skills_df_fixed.csv")
 
 test_read %>%
   ggplot() +
   geom_bar(aes(x=type))
+
+length(unique(test_read$skill))
